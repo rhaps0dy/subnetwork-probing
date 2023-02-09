93ca5e7afbf1ee2aa5302927d530a0f1e437dd21
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-02-09 17:22:19 +0000

    gpt2 example runs with masked hooked transformer

---
+++ code/masked_gpt2.py
@@ -0,0 +1,8 @@
+# import os 
+# os.chdir("/home/ubuntu/TransformerLens/transformer_lens/")
+
+from transformer_lens.HookedTransformer import MaskedHookedTransformer
+
+gpt2 = MaskedHookedTransformer.from_pretrained("gpt2")
+
+print(gpt2.is_masked)
