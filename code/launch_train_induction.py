7c83a2861e1a1a9e1b0866ac72b66706d3ddade1
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-04-09 23:29:42 -0700

    Launch induction

--- code/launch_train_induction.py
+++ code/launch_train_induction.py
@@ -3,24 +3,27 @@ import numpy as np
 import shlex
 import random
 
-regularization_params = 10**np.linspace(-2, 3, 36)
-seed = random.randint(0, 2**31-1)
+regularization_params = 10 ** np.linspace(-2, 3, 36)
+seed = random.randint(0, 2**31 - 1)
 
 i = 0
 for lambda_reg in regularization_params:
     for zero_ablation in [0, 1]:
         for reset_target in [0, 1]:
-            command = shlex.join([
-                "python", "/Automatic-Circuit-Discovery/subnetwork-probing/code/train_induction.py",
-                f"--lambda_reg={lambda_reg:.3f}",
-                "--wandb-entity=adria-garriga",
-                "--wandb-group=regularization",
-                "--device=cuda",
-                "--epochs=3000",
-                f"--zero-ablation={zero_ablation}",
-                f"--reset-target={reset_target}",
-                f"--seed={seed}",
-            ])
+            command = shlex.join(
+                [
+                    "python",
+                    "/Automatic-Circuit-Discovery/subnetwork-probing/code/train_induction.py",
+                    f"--lambda-reg={lambda_reg:.3f}",
+                    "--wandb-entity=adria-garriga",
+                    "--wandb-group=regularization",
+                    "--device=cuda",
+                    "--epochs=3000",
+                    f"--zero-ablation={zero_ablation}",
+                    f"--reset-target={reset_target}",
+                    f"--seed={seed}",
+                ]
+            )
             print("Launching", command)
 
             subprocess.call(
@@ -28,9 +31,9 @@ for lambda_reg in regularization_params:
                     "ctl",
                     "job",
                     "run",
-                    f"--name=agarriga-sp-{i:02d}"
+                    f"--name=agarriga-sp-{i:02d}",
                     "--shared-host-dir-slow-tolerant",
-                    "--container=ghcr.io/rhaps0dy/automatic-circuit-discovery:0.2",
+                    "--container=ghcr.io/rhaps0dy/automatic-circuit-discovery:0.3",
                     "--cpu=4",
                     "--gpu=1",
                     "--login",
@@ -41,4 +44,3 @@ for lambda_reg in regularization_params:
                 ]
             )
             i += 1
-
