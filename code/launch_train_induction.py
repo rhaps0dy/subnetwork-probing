76e8de36da262a4b289cac0cae9617b3d1065ea4
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-04-09 23:43:39 -0700

    Remove double quoting

--- code/launch_train_induction.py
+++ code/launch_train_induction.py
@@ -39,7 +39,7 @@ for lambda_reg in regularization_params:
                     "--login",
                     "--wandb",
                     "--never-restart",
-                    f"--command='{command}'",
+                    f"--command={command}",
                     "--working-dir=/Automatic-Circuit-Discovery",
                 ]
             )
